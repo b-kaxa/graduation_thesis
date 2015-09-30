72683a498425ce4aa88246802dc6fc54c1bbf107
b-kaxa <nisigin@gmail.com>
2015-09-30 23:40:39 +0900

    create branch  view

--- 0924_rect/rect/build-tmp/source/rect.java
+++ 0924_rect/rect/build-tmp/source/rect.java
@@ -19,10 +19,47 @@ public void setup() {
     
     frameRate(60);
 
-    round = -250;
+    round = -400;
 }
 
 public void draw() {
+    view_init();
+
+    // \u659c\u3081\u306e\u9577\u65b9\u5f62\u8a2d\u7f6e
+    fill(255,255,255);
+    rotate(PI/4);
+    noStroke();
+    rect(0, 300, 150, 500);
+
+    // \u659c\u3081\u306e\u9577\u65b9\u5f62\u8a2d\u7f6e
+    fill(255,255,255);
+    rotate(-PI/2);
+    noStroke();
+    rect(0, 300, 150, 500);
+
+    // \u5ea7\u6a19\u3092\u623b\u3059
+    rotate(PI/4);
+
+
+
+
+    // \u7e26\u306e\u9577\u65b9\u5f62\u8a2d\u7f6e
+    fill(255,255,255);
+    noStroke();
+    rect(0, -150, 175, 500);
+
+    // \u52d5\u304f\u5186\u8a2d\u7f6e
+    fill(0,0,255);
+    if (round < 0) {
+        round += 1;
+    }
+    ellipse(0,round,100,100);
+
+    // \u5ea7\u6a19\u3092\u56de\u8ee2\u3001PI=180\u00b0
+    // rotate(PI/2);
+}
+
+public void view_init() {
     // \u80cc\u666f\u8272rgb
     background(255, 218, 185);
 
@@ -31,19 +68,6 @@ public void draw() {
 
     // \u56f3\u5f62\u306e\u5ea7\u6a19\u8ef8\u3092\u4e2d\u592e\u306b\u5909\u66f4
     rectMode(CENTER);
-
-    // \u5ea7\u6a19\u8ef8\u3092\u56de\u8ee2\u3055\u305b\u308b
-    // rotate(radians(frameCount));
-
-    // \u9577\u65b9\u5f62\u8a2d\u7f6e
-    rect(0, -100, 100, 500);
-
-    // \u5186\u8a2d\u7f6e
-    round += 1;
-    ellipse(0,round,100,100);
-
-    // \u5ea7\u6a19\u3092\u56de\u8ee2\u3001PI=180\u00b0
-    // rotate(PI/2);
 }
   public void settings() {  size(700,700); }
   static public void main(String[] passedArgs) {
--- 0924_rect/rect/rect.pde
+++ 0924_rect/rect/rect.pde
@@ -3,10 +3,47 @@ void setup() {
     size(700,700);
     frameRate(60);
 
-    round = -250;
+    round = -400;
 }
 
 void draw() {
+    view_init();
+
+    // 斜めの長方形設置
+    fill(255,255,255);
+    rotate(PI/4);
+    noStroke();
+    rect(0, 300, 150, 500);
+
+    // 斜めの長方形設置
+    fill(255,255,255);
+    rotate(-PI/2);
+    noStroke();
+    rect(0, 300, 150, 500);
+
+    // 座標を戻す
+    rotate(PI/4);
+
+
+
+
+    // 縦の長方形設置
+    fill(255,255,255);
+    noStroke();
+    rect(0, -150, 175, 500);
+
+    // 動く円設置
+    fill(0,0,255);
+    if (round < 0) {
+        round += 1;
+    }
+    ellipse(0,round,100,100);
+
+    // 座標を回転、PI=180°
+    // rotate(PI/2);
+}
+
+void view_init() {
     // 背景色rgb
     background(255, 218, 185);
 
@@ -15,17 +52,4 @@ void draw() {
 
     // 図形の座標軸を中央に変更
     rectMode(CENTER);
-
-    // 座標軸を回転させる
-    // rotate(radians(frameCount));
-
-    // 長方形設置
-    rect(0, -100, 100, 500);
-
-    // 円設置
-    round += 1;
-    ellipse(0,round,100,100);
-
-    // 座標を回転、PI=180°
-    // rotate(PI/2);
 }
